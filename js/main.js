c44b0465711ca1a15a9ab151071828d122a9686e
Brian O'Toole <brian@sparxoo.com>
2017-10-16 17:16:53 -0400

    updated header + added toggle nav

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 // Main.js
-/*
 
-$(function() {
-
+$('#nav-toggle').on('click', function() {
+  $('body').toggleClass('nav-open');
+  //$('.line').toggleClass('fillWidth');
 });
 
+$(document).keyup(function(e) {
+  var body = $('body');
+  if (body.is('.nav-open') && e.keyCode == '27') {
+    $('body').toggleClass('nav-open');
+  }
+});
+
+// get shape of svg for anim
+/*
+var shape = document.querySelector('#lines');
+var shapeLength = shape.getTotalLength();
+console.log(shapeLength); //1689.4237060546875
 */
